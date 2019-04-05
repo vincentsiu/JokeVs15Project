af70bfa2661aaec313320027499483fec3becb15
Vincent Siu <vincent.siu@flir.com>
2019-04-04 17:28:26 -0700

    Adding code

---
+++ JokeVs15Project/JokeVs15Project.cpp
@@ -0,0 +1,12 @@
+// JokeVs15Project.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "HELLO" << std::endl;
+    return 0;
+}
+
